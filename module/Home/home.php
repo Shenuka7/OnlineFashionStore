bf9fa42ee6b6d25dc59bb0cc5262194ec437765b
Shenuka Gunathilake <shenukagunathilake5@gmail.com>
2022-08-11 00:11:23 +0530

    Create home.php

---
+++ module/Home/home.php
@@ -0,0 +1,471 @@
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TWERK LK&#x1F351 Online Fashion Store</title>
+    
+    <link rel = "icon" href = "../../twerk-4.png" type = "image/x-icon">
+    
+<!--    <style>
+        *{
+            padding:0;
+            margin:0;
+            box-sizing: border-box;
+        }
+        
+        body{
+            background:#eee;
+            font-family:sans-serif;
+        }
+        h1{
+  background:url('giphy.gif') center center no-repeat;
+  //background:url('https://media.giphy.com/media/FE0WTM8BG754I/giphy.gif') center center no-repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size:7rem;
+  letter-spacing:10px;
+  background-size:cover;
+  margin:10px;
+  color:transparent;
+  
+}
+
+.btn-hover {
+    width: 200px;
+    font-size: 16px;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+    margin: 20px;
+    height: 55px;
+    text-align:center;
+    border: none;
+    background-size: 300% 100%;
+
+    border-radius: 50px;
+    moz-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    -webkit-transition: all .4s ease-in-out;
+    transition: all .4s ease-in-out;
+}
+
+.btn-hover:hover {
+    background-position: 100% 0;
+    moz-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    -webkit-transition: all .4s ease-in-out;
+    transition: all .4s ease-in-out;
+}
+
+.btn-hover:focus {
+    outline: none;
+}
+
+.btn-hover.color-9 {
+    background-image: linear-gradient(to right, #25aae1, #4481eb, #04befe, #3f86ed);
+    box-shadow: 0 4px 15px 0 rgba(65, 132, 234, 0.75);
+}
+.btn-hover.color-7 {
+    background-image: linear-gradient(to right, #6253e1, #852D91, #A3A1FF, #F24645);
+    box-shadow: 0 4px 15px 0 rgba(126, 52, 161, 0.75);
+}
+.btn-hover.color-10 {
+        background-image: linear-gradient(to right, #ed6ea0, #ec8c69, #f7186a , #FBB03B);
+    box-shadow: 0 4px 15px 0 rgba(236, 116, 149, 0.75);
+}
+.btn-hover.color-11 {
+       background-image: linear-gradient(to right, #eb3941, #f15e64, #e14e53, #e2373f);  box-shadow: 0 5px 15px rgba(242, 97, 103, .4);
+}
+.btn-hover.color-3 {
+    background-image: linear-gradient(to right, #667eea, #764ba2, #6B8DD6, #8E37D7);
+    box-shadow: 0 4px 15px 0 rgba(116, 79, 168, 0.75);
+}
+.btn-hover.color-9 {
+    background-image: linear-gradient(to right, #25aae1, #4481eb, #04befe, #3f86ed);
+    box-shadow: 0 4px 15px 0 rgba(65, 132, 234, 0.75);
+}
+#about{
+text-align: center;
+}
+.about{
+  font-size: 24px;
+}
+.about:hover{
+  text-decoration: underline;
+}
+        
+        .banner-area{
+            width:100%;
+            height:100vh;
+            background-image: url(bg9.jpg);
+            background-size:cover;
+        }        
+        
+        .content-area{
+            height:100%;
+            display:flex;
+            justify-content: center;
+            align-items: center;    
+        }
+        
+       .content{
+            text-align: center;
+        }
+        
+        .content h1{
+            font-size:80px;
+            color:#fff;
+        }
+        
+        .content p{
+            font-size:30px;
+            color:#fff;
+        }
+        
+        .content .btn{
+            border:none;
+            outline:none;
+            padding:15px;
+            margin-top:100px;
+            border-radius: 4px;
+            color:#eee;
+            font-size: 18px;
+            cursor: pointer;    
+            background:#222;             
+        }
+        
+        /* main area */
+        
+        .container {
+            width: 70%;
+            margin: 50px auto;
+            padding: 10px;
+}
+
+        .container .box {
+            width: 100%;
+            padding: 50px;
+            background: #fff;
+            margin-top: 50px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px -3px rgba(0, 0, 0, .1);
+        }
+    </style>
+</head>
+<body>
+    <div class="banner-area">
+        <div class="content-area">
+           <div class="content">
+              <h1>ASOS ONLINE</h1>
+              
+              <p><b>Online Fashion Store</b></p>
+              <br>
+              <p id="about"></p>
+               <p id="about"></p>
+              <a href="../login/view/userlogin.php"><button class="btn-hover color-9">User Management</button>
+               <img src="qr7.png" alt="Copyright Qr" width="100" height="100">
+              
+             <a href="../../module/login/view/login/login-user.php"><button class="btn-hover color-3">Customer Portal</button>
+              
+              
+			  
+           </div>
+        </div>
+    </div>
+    
+    
+main area
+   
+   
+
+    
+</body>-->
+    <style>
+        body {
+    background-image: url("home4.jpg");
+    background-position: center;
+     width:100%;
+    height:100vh;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+}
+        *{
+            padding:0;
+            margin:0;
+            box-sizing: border-box;
+        }
+        
+        h1{
+  background:url('giphy.gif') center center no-repeat;
+  background:url('https://media.giphy.com/media/FE0WTM8BG754I/giphy.gif') center center no-repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke-width: 0.5px;
+  -webkit-text-stroke-color: black;
+  font-size:7rem;
+  letter-spacing:10px;
+  background-size:cover;
+  margin:10px;
+  color:transparent;
+        }
+  p{
+  -webkit-text-stroke-width: 0.5px;
+  -webkit-text-stroke-color: black;
+  
+}
+
+.btn-hover {
+    width: 200px;
+    font-size: 16px;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+    margin: 20px;
+    height: 55px;
+    text-align:center;
+    border: none;
+    background-size: 300% 100%;
+
+    border-radius: 50px;
+    moz-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    -webkit-transition: all .4s ease-in-out;
+    transition: all .4s ease-in-out;
+}
+
+.btn-hover:hover {
+    background-position: 100% 0;
+    moz-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    -webkit-transition: all .4s ease-in-out;
+    transition: all .4s ease-in-out;
+}
+
+.btn-hover:focus {
+    outline: none;
+}
+
+.btn-hover.color-9 {
+    background-image: linear-gradient(to right, #25aae1, #4481eb, #04befe, #3f86ed);
+    box-shadow: 0 4px 15px 0 rgba(65, 132, 234, 0.75);
+}
+.btn-hover.color-7 {
+    background-image: linear-gradient(to right, #6253e1, #852D91, #A3A1FF, #F24645);
+    box-shadow: 0 4px 15px 0 rgba(126, 52, 161, 0.75);
+}
+.btn-hover.color-10 {
+        background-image: linear-gradient(to right, #ed6ea0, #ec8c69, #f7186a , #FBB03B);
+    box-shadow: 0 4px 15px 0 rgba(236, 116, 149, 0.75);
+}
+.btn-hover.color-11 {
+       background-image: linear-gradient(to right, #eb3941, #f15e64, #e14e53, #e2373f);  box-shadow: 0 5px 15px rgba(242, 97, 103, .4);
+}
+.btn-hover.color-3 {
+    background-image: linear-gradient(to right, #667eea, #764ba2, #6B8DD6, #8E37D7);
+    box-shadow: 0 4px 15px 0 rgba(116, 79, 168, 0.75);
+}
+.btn-hover.color-9 {
+    background-image: linear-gradient(to right, #25aae1, #4481eb, #04befe, #3f86ed);
+    box-shadow: 0 4px 15px 0 rgba(65, 132, 234, 0.75);
+}
+#about{
+text-align: center;
+}
+.about{
+  font-size: 24px;
+}
+.about:hover{
+  text-decoration: underline;
+}
+        
+        .banner-area{
+            width:100%;
+            height:100vh;
+            margin-top: auto;
+            margin-bottom: auto;
+            
+            
+        }  
+        *{
+            font-family: 'Poppins', sans-serif;
+        }
+        
+        .content-area{
+            height:100%;
+            display:flex;
+            justify-content: center;
+            align-items: center;    
+        }
+        
+       .content{
+            text-align: center;
+        }
+        
+        .content h1{
+            
+            color:#fff;
+            font-size: 80px;
+            font-weight: 600;
+            font-family: 'Poppins', sans-serif;
+        }
+        .content h1:hover{
+            -webkit-text-stroke: 2px #fff;
+            color: transparent;
+        }
+        .content p{
+            font-size:30px;
+            color:#fff;
+        }
+        
+        .content .btn{
+            border:none;
+            outline:none;
+            padding:15px;
+            margin-top:100px;
+            border-radius: 4px;
+            color:#eee;
+            font-size: 18px;
+            cursor: pointer;    
+            background:#222;             
+        }
+        
+        /* main area */
+        
+        .container {
+            width: 70%;
+            margin: 50px auto;
+            padding: 10px;
+}
+
+        .container .box {
+            width: 100%;
+            padding: 50px;
+            background: #fff;
+            margin-top: 50px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px -3px rgba(0, 0, 0, .1);
+        }
+        
+        .back-video{
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            z-index: -1;
+            
+        }
+        
+        @media (max-aspect-ratio: 16/9){
+            .back-video{
+                width: 100%;
+                height: auto;
+            }
+        }
+        
+        .container h1{
+            font-size: 80px;
+            font-weight: 600;
+        }
+        span{
+            color: #fff724;
+        }
+        
+</style>
+
+   <body>
+     <div class="banner-area">
+        <div class="content-area">
+           <div class="content">
+               <p> &nbsp;</p>
+               <p> &nbsp;</p>
+               <p> &nbsp;</p>
+               
+              
+               <h1>TWERK LK</h1>
+               <p><b>Online Fashion Store</b></p>
+<!--               <video autoplay loop muted plays-inline class="back-video">
+                   <source src ="video.mp4" type="video/mp4">
+               </video>-->
+
+<!--               //<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>-->
+               
+               
+<!--               <script>
+                   var typed = new Typed(".auto-type", {
+                       strings: ["Coding", "Sleeping", "Eating"],
+                       typeSpeed: 150,
+                       backSpeed: 150,
+                       loop: true
+                   })
+               </script>
+               -->
+              <br>
+              <p id="about"></p>
+               <p id="about"></p>
+               <br>
+               <br>
+               <p> &nbsp;</p>
+               <p> &nbsp;</p>
+               <a href="../login/view/userlogin.php"><button class="btn-hover color-9">User Portal</button></a>
+              
+              
+               <a href="../../module/login/view/login/login-user.php"><button class="btn-hover color-3">Customer Portal</button></a>
+              
+              
+			  
+           </div>
+        </div>
+    </div>  
+  
+   
+   </body>
+   
+<!--  <script>
+var i = 0;
+var images = [];
+var slideTime = 3000; // 3 seconds
+
+images[0] = 'dash2.jpg';
+images[1] = 'dash3.jpg';
+images[2] = 'dash4.jpg';
+
+function changePicture() {
+    document.body.style.backgroundImage = "url(" + images[i] + ")";
+
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout(changePicture, slideTime);
+}
+
+window.onload = changePicture;
+</script>-->
+
+</html>
+
+
+<!-- <body>
+     <div class="banner-area">
+        <div class="content-area">
+           <div class="content">
+              <h1>TWERK LK&#x1F351</h1>
+              
+              <p><b>Online Fashion Store</b></p>
+              <br>
+              <p id="about"></p>
+               <p id="about"></p>
+              <a href="../login/view/userlogin.php"><button class="btn-hover color-9">User Portal</button>
+               <img src="qr7.png" alt="Copyright Qr" width="100" height="100">
+              
+             <a href="../../module/login/view/login/login-user.php"><button class="btn-hover color-3">Customer Portal</button>
+              
+              
+			  
+           </div>
+        </div>
+    </div>  
+  
+   
+   </body>-->
